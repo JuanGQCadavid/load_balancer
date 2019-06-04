29c3043580c76f1fa37cff8ef44b100c2a8eb2a4
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-06-04 15:01:46 -0500

    Fixed a problem with sockets

--- lb.py
+++ lb.py
@@ -2,12 +2,13 @@
 
 import socket
 import queue
+import _thread
 HOST = 'localhost'
-PORT = 8001 #Port listening -> (1023,65535]
+PORT = 8000 #Port listening -> (1023,65535]
 BACK_LOG = 100
 MAX_BUFFER = 65507 #https://es.stackoverflow.com/questions/43482/python-socket-como-recibir-todos-los-datos-con-socket-recv
 
-server_farm = {'server_1':{'ip':'3.84.69.127','port':8080},
+server_farm = {'server_1':{'ip':'3.84.69.127','port':80},
                'server_2':{'ip':'3.84.69.127', 'port':5000}
               }
 # socket.AF_INET -> Ipv4, socket.SOCK_STREAM -> TCP
@@ -41,41 +42,14 @@ def round_robin(servers_queue,global_socket):
     while True:
         conn,addr = global_socket.accept()
         print('Connected by', addr)
-
-        msg = b''
-        while True:
-            data = conn.recv(1024)
-            msg += data
-            if data[len(data)-1] == 10:
-                break
-        message = msg.decode("utf-8") 
-
-        print('-'*50)
-        print(message)
-        #Send the data
         server_to_send = servers_queue.get()
 
         server_ip = server_to_send['ip']
         server_port = server_to_send['port']
 
-        socket_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket_to_server.connect((server_ip, server_port))
-        socket_to_server.sendall(msg)
-        
-        response = b''
-        while True:
-            data = socket_to_server.recv(1024)
-            response += data
-            if data[len(data)-1] == 10:
-                break
-        message = response.decode("utf-8") 
+        print('Routing to', server_ip,':',server_port)
 
-        print(message)
-        print('-'*50)
-
-        conn.sendall(response)
-        conn.close()
-        socket_to_server.close()
+        _thread.start_new_thread( new_connection, (conn,addr,server_ip,server_port, ) )
         
         #add again the server
         servers_queue.put(server_to_send)
@@ -85,7 +59,56 @@ def round_robin(servers_queue,global_socket):
 
     pass
 
+def new_connection(conn,addr,server_ip,server_port):
+    #while True:
+        msg = b''
+        while True:
+            data = conn.recv(1024)
+            msg += data
 
+            if len(data) == 0:
+                break
+            #if data[len(data)-1] == 10:
+            if data[len(data)-1] == 10:
+                break
+        message = msg.decode("utf-8") 
+
+        print('-'*50)
+        message = message.replace('Connection: keep-alive','')
+        print(message)
+        print(len(message))
+        #if (len(message) == 0):
+        #    break
+        
+
+        
+        #message.replace('Host')
+        
+        #Send the data
+
+
+        socket_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_to_server.connect((server_ip, server_port))
+        socket_to_server.sendall(msg)
+        
+        response = b''
+        while True:
+            data = socket_to_server.recv(1024)
+            response += data
+            if len(data) == 0:
+                break
+            if data[len(data)-1] == 10:
+                break
+        message = response.decode("utf-8") 
+
+        print(message)
+        print('-'*50)
+
+        conn.sendall(response)
+        socket_to_server.close()
+
+        conn.close()
+    
 
 if __name__ == '__main__':
     main()
