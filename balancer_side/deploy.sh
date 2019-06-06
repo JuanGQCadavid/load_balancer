a7a85486ed20a22b9b9170430b522b866213a358
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-06-06 17:25:16 -0500

    Updating load balancer deploy script

--- balancer_side/deploy.sh
+++ balancer_side/deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+echo "Cheking if docker is already installed"
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Docker is no isnatlled.'
+  wget -qO- https://get.docker.com/ | sh
+fi
+
+echo "Building load balancer image ..."
 docker build -t load-balancer-img .
+echo "Done."
+echo "Staring service load balancer at port 80 ..."
 docker run -d --name load-balancer-service -p 80:9000 load-balancer-img
+echo "Done."
