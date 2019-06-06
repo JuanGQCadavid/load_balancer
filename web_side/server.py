8eb77363b4876e934d9fa6b4bab2092214a006b0
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-06-05 23:05:46 -0500

    Web page done, missing connection with database

--- web_side/server.py
+++ web_side/server.py
@@ -1,12 +1,47 @@
-from flask import Flask,render_template, request,jsonify,Response
+from flask import Flask,render_template, request,jsonify,Response,redirect,url_for
 
 # Router calls objetc
 app = Flask(__name__)
 
 #Add a single endpoint - testing
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if valid_login(username,password):
+            return redirect(url_for('home'))
+        return render_template('index.html')
+
+    return render_template('index.html')
+
+@app.route('/home', methods = ['GET'])
 def home():
-    return render_template('log.html')
+    return render_template('home.html')
+
+@app.route('/register', methods = ['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        correo = request.form['correo']
+        password = request.form['password']
+
+        register_user(username,password,correo)
+
+        return redirect(url_for('index'))
+    return render_template('register.html')
+
+def valid_login(username,password):
+    print('Log-in ->' , username, password)
+    if(username == 'juan') and (password == '123'):
+        return True
+    else:
+        return False
+
+def register_user(username,password,email):
+    print('Save ->',username,password,email)
+    pass
 
 #When run from command line, start the server
 if __name__ == '__main__':
