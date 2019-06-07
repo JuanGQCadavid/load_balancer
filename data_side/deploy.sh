3496c7139df9e59bd1ac3eb89aeee908791322d5
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-06-07 09:48:01 -0500

    Fixing typo

--- data_side/deploy.sh
+++ data_side/deploy.sh
@@ -8,4 +8,4 @@ fi
 docker volume create mysql-db-data
 docker volume ls
 docker run -d -p 33060:3306 --name app-mysql-db -e MYSQL_ROOT_PASSWORD=soylaclavesecreta --mount src=mysql-db-data,dst=/var/lib/mysql mysql
-docker exec -it mysql-db mysql -p
+docker exec -it app-mysql-db mysql -p
