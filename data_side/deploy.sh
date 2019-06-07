6d436ab080fcbb4f842ab54b5b618fd841880326
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-06-07 09:41:09 -0500

    Data base deply script and user table

---
+++ data_side/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Cheking if docker is already installed"
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Docker is no isnatlled.'
+  wget -qO- https://get.docker.com/ | sh
+fi
+
+docker volume create mysql-db-data
+docker volume ls
+docker run -d -p 33060:3306 --name app-mysql-db -e MYSQL_ROOT_PASSWORD=soylaclavesecreta --mount src=mysql-db-data,dst=/var/lib/mysql mysql
+docker exec -it mysql-db mysql -p
